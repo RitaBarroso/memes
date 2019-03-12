731ba638bc553c570a260da482d4c3d07cf4e756
RitaBarroso <48486056+RitaBarroso@users.noreply.github.com>
2019-03-12 16:22:20 +0000

    added files

---
+++ script.js
@@ -0,0 +1,44 @@
+$(window).scroll(function() {
+
+  // selectors
+  var $window = $(window),
+      $body = $('body'),
+      $panel = $('.panel');
+
+  // Change 33% earlier than scroll position so colour is there when you arrive.
+  var scroll = $window.scrollTop() + ($window.height() / 3);
+
+  $panel.each(function () {
+    var $this = $(this);
+
+    // if position is within range of this panel.
+    // So position of (position of top of div <= scroll position) && (position of bottom of div > scroll position).
+    // Remember we set the scroll to 33% earlier in scroll var.
+    if ($this.position().top <= scroll && $this.position().top + $this.height() > scroll) {
+
+      // Remove all classes on body with color-
+      $body.removeClass(function (index, css) {
+        return (css.match (/(^|\s)color-\S+/g) || []).join(' ');
+      });
+
+      // Add class of currently active div
+      $body.addClass('color-' + $(this).data('color'));
+    }
+  });
+
+}).scroll();
+
+$(".concept").on("click", function() {
+  $(".concept").css("z-index", 0);
+  $(this).css("z-index", 1);
+});
+
+var figure = $(".video").hover( hoverVideo, hideVideo );
+
+function hoverVideo(e) {
+    $('video', this).get(0).play();
+}
+
+function hideVideo(e) {
+    $('video', this).get(0).pause();
+}
